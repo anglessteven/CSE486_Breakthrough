f1a36db6c61262a6caad26bf4745de4fe8c2c9bb
Steven Angles <anglessw@miamioh.edu>
2013-11-05 15:29:54 -0500

    multithreading started

--- Tournament/Insert_Text_Here/AlphaBetaMT.java
+++ Tournament/Insert_Text_Here/AlphaBetaMT.java
@@ -3,12 +3,242 @@
  */
 package Insert_Text_Here;
 
+import game.GameState;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import breakthrough.BreakthroughMove;
+import breakthrough.BreakthroughState;
+
 /**
  * @author Steven
  * 
  */
 public class AlphaBetaMT extends Thread {
-	public AlphaBetaMT() {
+	public final int MAX_DEPTH = 50;
+	public static final int MAX_SCORE = Integer.MAX_VALUE;
+	protected ScoredBreakthroughMove[] mvStack;
+	private ArrayList<BreakthroughMove> moves;
+	private int start, end, depthLimit;
+	private BreakthroughState brd;
+	
+	public AlphaBetaMT(int start, int end, int depthLimit, BreakthroughState brd, ArrayList<BreakthroughMove> moves) {
+		this.start = start;
+		this.end = end;
+		this.brd = brd;
+		this.depthLimit = depthLimit;
+		this.moves = moves;
 		
+		mvStack = new ScoredBreakthroughMove[MAX_DEPTH];
+		for (int i = 0; i < MAX_DEPTH; i++) {
+			mvStack[i] = new ScoredBreakthroughMove(0, 0, 0, 0, 0);
+		}
+		
+		alphaBeta(this.brd, 0, Double.NEGATIVE_INFINITY,
+				Double.POSITIVE_INFINITY, true);
+	}
+	
+	
+	private void alphaBeta(BreakthroughState brd, int currDepth, double alpha,
+			double beta, boolean firstLevel) {
+		boolean toMaximize = (brd.getWho() == GameState.Who.HOME);
+		boolean toMinimize = !toMaximize;
+
+		boolean isTerminal = terminalValue(brd, mvStack[currDepth]);
+
+		if (isTerminal) {
+			return;
+		} else if (currDepth == depthLimit) {
+			mvStack[currDepth].setScore(evalBoard(brd));
+		} else {
+			double bestScore = (toMaximize ? Double.NEGATIVE_INFINITY
+					: Double.POSITIVE_INFINITY);
+			ScoredBreakthroughMove bestMove = mvStack[currDepth];
+			ScoredBreakthroughMove nextMove = mvStack[currDepth + 1];
+
+			bestMove.setScore(bestScore);
+			GameState.Who currTurn = brd.getWho();
+			
+			char me = brd.who == BreakthroughState.Who.HOME ? BreakthroughState.homeSym
+					: BreakthroughState.awaySym;
+			int dir = brd.who == BreakthroughState.Who.HOME ? +1 : -1;
+			ArrayList<BreakthroughMove> moves;
+			
+			if(firstLevel){
+				moves = this.moves;
+			} else {
+				// Find valid moves
+				moves = new ArrayList<BreakthroughMove>();
+				BreakthroughMove mv = new BreakthroughMove();
+				for (int r = 0; r < BreakthroughState.N; r++) {
+					for (int c = 0; c < BreakthroughState.N; c++) {
+						mv.startRow = r;
+						mv.startCol = c;
+						if (brd.board[r][c] == me) {
+							mv.endingRow = r + dir;
+							mv.endingCol = c;
+							if (brd.moveOK(mv)) {
+								// System.out.println("Move: " + mv.endingRow +
+								// " | " + mv.endingCol);
+								moves.add((BreakthroughMove) mv.clone());
+							}
+							mv.endingRow = r + dir;
+							mv.endingCol = c + 1;
+							if (brd.moveOK(mv)) {
+								moves.add((BreakthroughMove) mv.clone());
+								// System.out.println("Move: " + mv.endingRow +
+								// " | " + mv.endingCol);
+							}
+							mv.endingRow = r + dir;
+							mv.endingCol = c - 1;
+							if (brd.moveOK(mv)) {
+								moves.add((BreakthroughMove) mv.clone());
+								// System.out.println("Move: " + mv.endingRow +
+								// " | " + mv.endingCol);
+							}
+						}
+					}
+				}
+			}
+			Collections.shuffle(moves);
+			int i = (firstLevel) ? this.start : 0;
+			int end = (firstLevel) ? this.end : moves.size();
+			for (; i < end; i++) {
+				BreakthroughMove tempMv = moves.get(i);
+				// Before move, store what type of board square existed there
+				char prevPiece = brd.board[tempMv.endingRow][tempMv.endingCol];
+				brd.makeMove(tempMv);
+
+				alphaBeta(brd, currDepth + 1, alpha, beta, false); // Check out
+															// move
+
+				// Undo move
+				brd.board[tempMv.endingRow][tempMv.endingCol] = prevPiece;
+				brd.board[tempMv.startRow][tempMv.startCol] = me;
+				brd.numMoves--;
+				brd.status = GameState.Status.GAME_ON;
+				brd.who = currTurn;
+
+				// Check out the results, relative to what we've seen before
+				if (toMaximize && nextMove.score > bestMove.score) {
+					bestMove.set(tempMv.startRow, tempMv.startCol,
+							tempMv.endingRow, tempMv.endingCol, nextMove.score);
+				} else if (!toMaximize && nextMove.score < bestMove.score) {
+					bestMove.set(tempMv.startRow, tempMv.startCol,
+							tempMv.endingRow, tempMv.endingCol, nextMove.score);
+				}
+
+				// Update alpha and beta. Perform pruning, if possible.
+				if (toMinimize) {
+					beta = Math.min(bestMove.score, beta);
+					if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+						return;
+					}
+				} else {
+					alpha = Math.max(bestMove.score, alpha);
+					if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Determines if a board represents a completed game. If it is, the
+	 * evaluation values for these boards is recorded (i.e., 0 for a draw +X,
+	 * for a HOME win and -X for an AWAY win.
+	 * 
+	 * @param brd
+	 *            Breakthrough board to be examined
+	 * @param mv
+	 *            where to place the score information; column is irrelevant
+	 * @return true if the brd is a terminal state
+	 */
+	protected boolean terminalValue(GameState brd, ScoredBreakthroughMove mv) {
+		GameState.Status status = brd.getStatus();
+		boolean isTerminal = true;
+
+		if (status == GameState.Status.HOME_WIN) {
+			mv.setScore(MAX_SCORE); // 0?
+			// System.out.println("HOME_WIN:" + mv.score);
+		} else if (status == GameState.Status.AWAY_WIN) {
+			mv.setScore(-MAX_SCORE);
+			// System.out.println("AWAY_WIN:" + mv.score);
+		} else if (status == GameState.Status.DRAW) {
+			mv.setScore(0);
+		} else {
+			isTerminal = false;
+		}
+		return isTerminal;
+	}
+
+	/**
+	 * Counts the number of adjacent pairs of spots with same player's piece.
+	 * 
+	 * @param brd
+	 *            board to be evaluated
+	 * @param who
+	 *            'R' or 'B'
+	 * @return number of adjacent pairs equal to who
+	 */
+	private static int eval(BreakthroughState brd, char who) {
+		// initializing all the variables I need on order to count the score,
+		int score = 0;
+
+		// various eval functions for loop
+		// ideas: check different offensive/defensive configs?
+		// check furthest player?
+		// clumping
+		for (int r = 0; r < BreakthroughState.N; r++) {
+			for (int c = 0; c < BreakthroughState.N; c++) {
+				int left = c - 1;
+				int right = c + 1;
+				/* Check for x_x_x in last row */
+				if (left >= 0 && right < BreakthroughState.N) {
+					if (brd.board[r][left] == BreakthroughState.emptySym
+							&& brd.board[r][right] == BreakthroughState.emptySym) {
+						if ((who == BreakthroughState.awaySym
+								&& brd.board[r][c] == who && r == 6)
+								|| (who == BreakthroughState.homeSym
+										&& brd.board[r][c] == who && r == 0)) {
+							score--;
+						}
+					}
+				}
+				/* check adjacent */
+				int next = c + 1;
+				if (next < BreakthroughState.N && brd.board[r][c] == who
+						&& brd.board[r][next] == who)
+					score++;
+				/* check behind */
+				int nextFront = r + 1;
+				if (nextFront < BreakthroughState.N && brd.board[r][c] == who
+						&& brd.board[nextFront][c] == who)
+					score++;
+				/* value having more players */
+				if (who == brd.board[r][c])
+					score++;
+			}
+		}
+		return score;
+	}
+
+	/**
+	 * The evaluation function
+	 * 
+	 * @param brd
+	 *            board to be evaluated
+	 * @return Home evaluation - Away evaluation
+	 */
+	public static int evalBoard(BreakthroughState brd) {
+		int score = eval(brd, BreakthroughState.homeSym)
+				- eval(brd, BreakthroughState.awaySym);
+		if (Math.abs(score) > MAX_SCORE) {
+			System.err.println("Problem with eval");
+			System.exit(0);
+		}
+		return score;
 	}
 }
