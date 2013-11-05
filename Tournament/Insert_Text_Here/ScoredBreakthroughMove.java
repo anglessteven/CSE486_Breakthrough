b34b73d0347ee8dd5bb6b84468822782d25f80a2
Steven Angles <anglessw@miamioh.edu>
2013-11-05 16:02:47 -0500

    added BreakthroughMove.java

---
+++ Tournament/Insert_Text_Here/ScoredBreakthroughMove.java
@@ -0,0 +1,35 @@
+package Insert_Text_Here;
+
+import breakthrough.BreakthroughMove;
+
+public class ScoredBreakthroughMove extends BreakthroughMove{
+
+	public ScoredBreakthroughMove(int r1, int c1, int r2, int c2, double s) {
+		super(r1, c1, r2, c2);
+		score = s;
+	}
+
+	public ScoredBreakthroughMove() {
+		super(0, 0, 0, 0);
+		score = 0;
+	}
+
+	public void set(int r1, int c1, int r2, int c2, double s) {
+		startRow = r1;
+		startCol = c1;
+		endingRow = r2;
+		endingCol = c2;
+		score = s;
+	}
+
+	public void setScore(double s) {
+		score = s;
+	};
+
+	public Object clone() {
+		return new ScoredBreakthroughMove(startRow, startCol, endingRow,
+				endingCol, score);
+	}
+
+	public double score;
+}
